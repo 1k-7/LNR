bd2e2fdb298f95cf3f8039dbafa1a5808dd48209
Aldrin Rishi <perfect.kryptonian@gmail.com>
2025-09-26 11:03:34 +0530

    Delete lncrawl/core/app.py

--- lncrawl/core/app.py
+++
@@ -1,7 +0,0 @@
-# lncrawl/core/app.py
-
-class App:
-    def __init__(self):
-        self.user_input = None
-        self.crawler = None
-        # Add other necessary attributes from the original App class
